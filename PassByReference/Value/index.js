647dca6bd22ed091ecee2261c49bf895503761a8
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-05-17 00:34:06 +0530

    pass by ref and val

---
+++ PassByReference/Value/index.js
@@ -0,0 +1,16 @@
+// Primitive types (number, string, boolean) are passed by value (cant be changed by third party functions)
+// Reference types (objects) can be changed as they are passed by reference
+
+const a = 1;
+const name = {};
+function change(a, name){
+    a = 1;
+    name.firstName = 'Aman';
+    name.lastName = 'Bhardwaj';
+    // but the function cannot change what name object points to, it can only change its props not its dest.
+    name = { 'hell': 'no' };
+}
+
+change(a, name);
+console.log(a);
+console.log(name);
