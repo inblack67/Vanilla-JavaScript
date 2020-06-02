47e76d9e8878e29500d6a7a85d3a8564e741226d
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-06-02 23:02:49 +0530

    chaining

---
+++ Asynchronous JavaScript/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const zlibPromise = util.promisify(zlib.gzip);
+
+readFile('./node/files/demofile.txt', 'utf-8')
+  .then((data) =>
+    zlibPromise(data)
+      .then((data) => console.log(`Final: ${data}`))
+      .catch((err) => console.error(`Zip Failed: ${err}`))
+  )
+  .catch((err) => console.error(`Read Failed: ${err}`));
+
+// still nesting issues - promise then hell
