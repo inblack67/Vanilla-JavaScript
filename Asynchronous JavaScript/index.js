11292b9393b8fc4cab5c6271b88dcde2a36d7f08
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-06-05 09:50:06 +0530

    Damn ES6

--- Asynchronous JavaScript/index.js
+++ Asynchronous JavaScript/index.js
@@ -1,13 +1,19 @@
-function* asyncGenerator() {
-    for (let i = 0; i < 10; i++) {
-        yield Promise.resolve(i);
+const util = require("util");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+
+function* fileLoader(files) {
+    const promises = files.map(file => readFile(file, 'utf8'));
+    for (const i of promises) {
+        yield i;
     }
 }
 
-const it3 = asyncGenerator();
-
 (async () => {
-    for await (let num of it3) {
-        console.log(num);
-      }
+  for await (let contents of fileLoader([
+    "./node/files/demofile.txt",
+    "./node/files/demofile.other.txt"
+  ])) {
+    console.log(contents);
+  }
 })();
--- Asynchronous JavaScript/promises/generators.js
+++ Asynchronous JavaScript/promises/generators.js
@@ -42,3 +42,23 @@ const it3 = asyncGenerator();
         console.log(num);
       }
 })();
+
+const util = require("util");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+
+function* fileLoader(files) {
+    const promises = files.map(file => readFile(file, 'utf8'));
+    for (const i of promises) {
+        yield i;
+    }
+}
+
+(async () => {
+  for await (let contents of fileLoader([
+    "./node/files/demofile.txt",
+    "./node/files/demofile.other.txt"
+  ])) {
+    console.log(contents);
+  }
+})();
